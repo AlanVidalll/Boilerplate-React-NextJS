1745eaace08a9fd257d4f237a8bbc4827a18447c
Alan Vidal <alan.fonseca@kumulus.com.br>
2021-12-21 16:51:26 -0300

    plopfile modify dashcase to pascalcase

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,44 +1,3 @@
-/* module.exports = function (plop) {
-  plop.setGenerator('controller', {
-    description: 'application component',
-
-    // inquirer prompts
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Controller name?'
-      }
-    ],
-
-    // actions to perform
-    actions: [
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}//index.tsx',
-        templateFile: 'templates/index.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}//stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
-
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}//styles.ts',
-        templateFile: 'templates/styles.ts.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}//test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
-      }
-    ]
-  })
-}
- */
-
 module.exports = function (plop) {
   plop.setGenerator('component', {
     description: 'application component logic',
@@ -52,22 +11,22 @@ module.exports = function (plop) {
     actions: [
       {
         type: 'add',
-        path: '../src/components/{{dashCase name}}/index.tsx',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
         templateFile: 'templates/index.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{dashCase name}}/index.stories.tsx',
+        path: '../src/components/{{pascalCase name}}/index.stories.tsx',
         templateFile: 'templates/index.stories.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{dashCase name}}/index.test.tsx',
+        path: '../src/components/{{pascalCase name}}/index.test.tsx',
         templateFile: 'templates/index.test.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{dashCase name}}/styles.ts',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
       }
     ]
